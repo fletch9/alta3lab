b6e739c4acda60ec51ecebcc13d745138c45b3e3
fletch9 <fletch9us@yahoo.com>
2022-08-10 14:10:55 +0000

    rpg stuff

---
+++ new stuff.py
@@ -0,0 +1,12 @@
+## A dictionary linking a room to other rooms
+rooms = {
+
+            'Hall' : {
+                  'south' : 'Kitchen'
+                },
+
+            'Kitchen' : {
+                  'north' : 'Hall'
+                }
+
+         }

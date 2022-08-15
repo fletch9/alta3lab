ccddbbbf606e6b55830a01aeba5cc23547edf99f
fletch9 <fletch9us@yahoo.com>
2022-08-15 14:56:27 +0000

    monday chall

---
+++ stuff/README.md
@@ -0,0 +1,8 @@
+# mycode 
+training account
+just learning
+who knows
+just adding stuff
+
+
+We're comfortable with the world borrowing our code, provided they give us credit, should it get used.
---
+++ stuff/challange.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   CHALLENGE 01 - Solution"""    
+
+def main():
+
+    user_input = input("Please enter your name: ")
+    
+    ## the line below creates a single string that is passed to print()
+    # print("You told me the IPv4 address is:" + user_input)
+    
+    ## print() can be given a series of objects separated by a comma
+    print("Hello,:", user_input)
+    
+    # asking user for 'vendor name'
+    vendor = input("what day of the week is it: ")
+    print("Happy :", vendor)
+
+main()
